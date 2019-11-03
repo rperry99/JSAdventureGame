55dd507ca775ae1774feac2973a1ca63bf31cf55
Russ Perry <rperry1886@gmail.com>
2019-11-03 08:10:28 -0500

    Fix bug of SetAttribute of null

--- app.js
+++ app.js
@@ -8,8 +8,7 @@ MakeActive(playerSpace);
 
 function MakeActive(nextSpace) {
   let boxesNodeList = document.querySelectorAll(".game div");
-  for (let i = 1; i <= boxesNodeList.length + 1; i++) {
-    // boxesNodeList[i].classList.add("active");
+  for (let i = 1; i <= boxesNodeList.length; i++) {
     if (i === nextSpace) {
       boxesNodeList[nextSpace - 1].classList.add("active");
     } else {
@@ -19,24 +18,28 @@ function MakeActive(nextSpace) {
   console.log(nextSpace);
 }
 
+//When the user presses the left button
 function MoveLeft() {
   if (playerSpace != 1 || playerSpace != 4 || playerSpace != 7) {
     playerSpace -= 1;
     MakeActive(playerSpace);
   }
 }
+//When the user presses the up button
 function MoveUp() {
   if (playerSpace != 1 || playerSpace != 2 || playerSpace != 3) {
     playerSpace -= 3;
     MakeActive(playerSpace);
   }
 }
+//When the user presses the down button
 function MoveDown() {
   if (playerSpace != 7 || playerSpace != 8 || playerSpace != 9) {
     playerSpace += 3;
     MakeActive(playerSpace);
   }
 }
+//When the user presses the right button
 function MoveRight() {
   if (playerSpace != 3 || playerSpace != 6 || playerSpace != 9) {
     playerSpace += 1;
