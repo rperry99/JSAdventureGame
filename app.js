8a96c588c3a926b0f0a103f14516984809a00357
Russ Perry <rperry1886@gmail.com>
2019-11-03 08:18:12 -0500

    Fix bug where the player can go out of bounds

--- app.js
+++ app.js
@@ -20,28 +20,36 @@ function MakeActive(nextSpace) {
 
 //When the user presses the left button
 function MoveLeft() {
-  if (playerSpace != 1 || playerSpace != 4 || playerSpace != 7) {
+  if (playerSpace === 1 || playerSpace === 4 || playerSpace === 7) {
+    return;
+  } else {
     playerSpace -= 1;
     MakeActive(playerSpace);
   }
 }
 //When the user presses the up button
 function MoveUp() {
-  if (playerSpace != 1 || playerSpace != 2 || playerSpace != 3) {
+  if (playerSpace === 1 || playerSpace === 2 || playerSpace === 3) {
+    return;
+  } else {
     playerSpace -= 3;
     MakeActive(playerSpace);
   }
 }
 //When the user presses the down button
 function MoveDown() {
-  if (playerSpace != 7 || playerSpace != 8 || playerSpace != 9) {
+  if (playerSpace === 7 || playerSpace === 8 || playerSpace === 9) {
+    return;
+  } else {
     playerSpace += 3;
     MakeActive(playerSpace);
   }
 }
 //When the user presses the right button
 function MoveRight() {
-  if (playerSpace != 3 || playerSpace != 6 || playerSpace != 9) {
+  if (playerSpace === 3 || playerSpace === 6 || playerSpace === 9) {
+    return;
+  } else {
     playerSpace += 1;
     MakeActive(playerSpace);
   }
